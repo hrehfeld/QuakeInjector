8d30511d9d282113a7d1a08919c10552558d7755
Eric Wasylishen <ewasylishen@gmail.com>
2015-08-07 14:30:42 -0600

    Add a placeholder BuildCommit.java so you can work in eclipse without having to run an ant build. This file will be overwritten by an ant build with a real version string. It is still in the .gitignore so changes to it won't be noticed by git.

---
+++ src/de/haukerehfeld/quakeinjector/BuildCommit.java
@@ -0,0 +1,6 @@
+package de.haukerehfeld.quakeinjector;
+public final class BuildCommit { 
+      // this is so you can work in eclipse without having to run an ant build.
+      // this file will be overwritten by an ant build with a real version string.
+      public static final String buildCommit = "";
+}
